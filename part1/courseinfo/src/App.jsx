67a874878d371fe599727c3c36b487b37f31b951
mishevski-k <mishevski0800@gmail.com>
2023-03-13 19:45:46 +0100

    Refactored variables in App component to be one object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,27 +3,29 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 const App = () => {
-    const course = "Half stack application development";
-    const parts = [
-        {
-            "name": "Fundamentals of React",
-            "exercises": 10
-        },
-        {
-            "name": "Using props to pass data",
-            "exercises": 7
-        },
-        {
-            "name": "State of component",
-            "exercises": 14
-        }
-    ]
+    const course = {
+        name: 'Half stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of component',
+                exercises: 14
+            },
+        ]
+    };
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     );
 }
