732abc203642ac78902450f4f0525f7f997714eb
mishevski-k <mishevski0800@gmail.com>
2023-03-14 18:13:47 +0100

    Part1 exercise 1.7 add more statisctics to the rating system:

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,11 +4,35 @@ const App = () => {
   const [ good, setGood ] = useState(0);
   const [ neutral, setNeutral ] = useState(0);
   const [ bad, setBad ] = useState(0);
+  const [ ratingCount, setRatingCount ] = useState(0);
+  const [ avarage, setAvarage ] = useState(0);
+  const [ positive, setPositive ] = useState(0);
 
-  const increaseGoodByOne = () => setGood(good + 1);
-  const increaseNeutralByOne = () => setNeutral(neutral + 1);
-  const increaseBadByOne = () => setBad(bad + 1);
+  const increaseRatingCountByOne = () => setRatingCount(ratingCount + 1);
 
+  const increaseGoodByOne = () => {
+    increaseRatingCountByOne()
+    setGood(good + 1);
+    const newGood = good + 1;
+    const newRatingCount = ratingCount + 1;
+    setAvarage((newGood - bad) / newRatingCount);
+    setPositive((newGood / newRatingCount) * 100);
+  }
+  const increaseNeutralByOne = () => {
+    increaseRatingCountByOne()
+    setNeutral(neutral + 1);
+    const newRatingCount = ratingCount + 1;
+    setAvarage((good - bad) / newRatingCount);
+    setPositive((good / newRatingCount) * 100);
+  }
+  const increaseBadByOne = () => {
+    increaseRatingCountByOne()
+    setBad(bad + 1);
+    const newBad = bad + 1;
+    const newRatingCount = ratingCount + 1;
+    setAvarage((good - newBad) / newRatingCount);
+    setPositive((good / newRatingCount) * 100);
+  } 
   return(
     <div>
       <h1>Give feedback</h1>
@@ -22,6 +46,9 @@ const App = () => {
         <p>Good { good }</p>
         <p>Neutral { neutral }</p>
         <p>Bad { bad }</p>
+        <p>all { ratingCount }</p>
+        <p>avarage { avarage }</p>
+        <p>positive { positive } %</p>
       </div>
     </div>
   )
