be6d71b30089e2f7c4fa07928d968de8b6826bba
mishevski-k <mishevski0800@gmail.com>
2023-03-14 19:46:35 +0100

    Part1 exercise 1.14 added logic to display most voted anecdote

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -38,6 +38,8 @@ function App() {
 
   const [ selected, setSelected ] = useState(0);
   const [ anecdotes, setAnecdotes ] = useState(predifinedAnecdotes); 
+  const [ mostVotedAnecdote, setMostVotedAnecdote ] = useState(0);
+  const [ totalVotes, setTotalVotes ] = useState(0);
 
   function randomNumberMinMax(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
@@ -53,19 +55,25 @@ function App() {
   const voteAnecdote = () => {
     const copyAnecdotes = [ ...anecdotes ];
     copyAnecdotes[selected].votes += 1;
-    console.log(copyAnecdotes);
     setAnecdotes(copyAnecdotes);
-    console.log(anecdotes);
+    if(copyAnecdotes[selected].votes > copyAnecdotes[mostVotedAnecdote].votes){
+      setMostVotedAnecdote(selected);
+    }
+    setTotalVotes(totalVotes +  1);
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{ anecdotes[selected].content }</p>
       <p>has { anecdotes[selected].votes } votes</p>
       <div>
         <button onClick={voteAnecdote}>vote</button>
         <button onClick={nextAnecdote}>next anecdote</button>
       </div>
+      <h1>Anecdote with most votes</h1>
+      <p>{ anecdotes[mostVotedAnecdote].content }</p>
+      <p>has { anecdotes[mostVotedAnecdote].votes } votes</p>
     </div>
   )
 }
