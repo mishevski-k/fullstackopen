57669a2a1ad447703342c6c9e0ce7207d1ca290c
mishevski-k <mishevski0800@gmail.com>
2023-03-15 22:57:58 +0100

    Part2 exercise 2.9 added filter functionality to search users by substring of their name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,18 @@ import { useState } from 'react';
 import PhoneList from "./components/PhoneList";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([{name: 'Arto Hellas', id: 1, number: '030-12345678'}]);
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
+  const [ filterPerson, setFilterPerson ] = useState('');
+  const personsToShow = filterPerson === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filterPerson.toLowerCase()));
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -12,6 +21,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  }
+
+  const handleFilterPerson = (event) => {
+    setFilterPerson(event.target.value);
   }
 
   const addPerson = (event) => {
@@ -37,6 +50,10 @@ const App = () => {
   return(
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with <input value={filterPerson} onChange={handleFilterPerson} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -49,7 +66,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <PhoneList list={persons} />
+      <PhoneList list={personsToShow} />
     </div>
   )
 }
