43373889a21d82992060560281e4badb5ea89bc9
mishevski-k <mishevski0800@gmail.com>
2023-03-15 23:12:52 +0100

    Part2 exercise 2.10 refactored code into smaller components

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import PhoneList from "./components/PhoneList";
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
 
 const App = () => {
+  
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
+
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ filterPerson, setFilterPerson ] = useState('');
+
   const personsToShow = filterPerson === ''
     ? persons
     : persons.filter(person => person.name.toLowerCase().includes(filterPerson.toLowerCase()));
@@ -50,22 +55,10 @@ const App = () => {
   return(
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter show with <input value={filterPerson} onChange={handleFilterPerson} />
-      </div>
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+      <Filter value={filterPerson} handleChange={handleFilterPerson} />
+      <h3>Add a new</h3>
+      <PersonForm handleSubmit={addPerson} nameValue={newName} numberValue={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
+      <h3>Numbers</h3>
       <PhoneList list={personsToShow} />
     </div>
   )
