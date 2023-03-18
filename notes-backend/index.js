ced1c771bba911be519ba185543d4362abacea81
mishevski-k <mishevski0800@gmail.com>
2023-03-18 21:07:23 +0100

    Added error hendling for mongodb request and added update and delete opreations with mongodb

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -45,17 +45,18 @@ app.get('/api/notes', ( request, response ) => {
     })
 });
 
-app.get('/api/notes/:id', ( request, response ) => {
+app.get('/api/notes/:id', ( request, response, next ) => {
     const id = request.params.id;
     Note
         .findById(id)
         .then(note => {
-            response.json(note);
+            if(note){
+                response.json(note);
+            }else{
+                response.status(404).send({error: "Note cannot be found"});
+            }
         })
-        .catch((error) => {
-            response.status(404).send({error: "Note cannot be found"});
-
-        })
+        .catch(error => next(error) )
 });
 
 app.post('/api/notes', ( request, response) => {
@@ -77,11 +78,33 @@ app.post('/api/notes', ( request, response) => {
     });
 });
 
-app.delete('/api/notes/:id', ( request, response ) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-    
-    response.status(204).end();
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
+
+    if(!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        });
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', ( request, response, next ) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            reposen.status(204).end()
+        })
+        .catch(error => next(error))
 });
 
 const unknownEnpoint = (request, response) => {
@@ -89,6 +112,18 @@ const unknownEnpoint = (request, response) => {
 }
 
 app.use(unknownEnpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+
+    if(error.name === 'CastError'){
+        return response.status(400).send({error: 'malformed id'})
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
