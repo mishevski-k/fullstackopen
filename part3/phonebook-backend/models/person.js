ff26e16b3974cd80e5b40cbf321703e5a0ea585f
mishevski-k <mishevski0800@gmail.com>
2023-03-19 00:18:04 +0100

    Part3 exercise 3.20 added phone number validation with custom validator and displaying error messages

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: (v) =>  {
+                return /^\d{2}\d?-\d{5,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    },
 });
 
 personSchema.set('toJSON', {
