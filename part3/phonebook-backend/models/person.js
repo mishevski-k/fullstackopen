1c978e39b844c2542641fee6db5a27c3c5f95358
mishevski-k <mishevski0800@gmail.com>
2023-03-19 00:52:41 +0100

    Part3 exercise 3.22 Added eslint for dev dependancy and fixed most of errors showing up

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -8,11 +8,11 @@ console.log('connecting to', url);
 
 mongoose.connect(url)
     .then( result => {
-        console.log("Connected to mongo db");
+        console.log('Connected to mongo db');
     })
     .catch( (error) => {
-        console.log("error connecting to MongoDB: ", error.message)
-    })
+        console.log('error connecting to MongoDB: ', error.message);
+    });
 
 const personSchema = new mongoose.Schema({
     name: {
@@ -34,9 +34,9 @@ const personSchema = new mongoose.Schema({
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
     }
 });
 
