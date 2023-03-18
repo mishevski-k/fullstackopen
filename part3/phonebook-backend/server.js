ce0103fcd5afb5fad7b7857dfbe0d5ce4792e593
mishevski-k <mishevski0800@gmail.com>
2023-03-18 21:47:48 +0100

    Part3 exercise 3.18 changed info endpoitn to work with db and removed old implemnetion

--- part3/phonebook-backend/server.js
+++ part3/phonebook-backend/server.js
@@ -14,34 +14,6 @@ server.use(express.static('dist'));
 server.use(morgan(':method :url :status :res[content-length] - :response-time ms :json-body'));
 server.use(cors());
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Marry Poppendick",
-        number: "39-23-6423122",
-    },
-    {
-        id: 5,
-        name: "Maja Aleksovska",
-        number: "389-078-000-000",
-    },
-];
-
 server.get('/api/v1', (request, response) => {
     const routes = {
         root: '/',
@@ -53,9 +25,15 @@ server.get('/api/v1', (request, response) => {
     response.json(routes);
 });
 
-server.get('/info', (request, response) => {
+server.get('/info', (request, response, next) => {
 
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><h4>${new Date()}</h4>`);
+    Person
+        .find({})
+        .then( result => {
+            response.send(`<p>Phonebook has info for ${result.length} people</p><h4>${new Date()}</h4>`);
+        })
+        .catch(error => next(error))
+
 });
 
 const personsResource = '/api/v1/persons';
