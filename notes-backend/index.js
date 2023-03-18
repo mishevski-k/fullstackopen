7144f8f8e7f37fbb32312f92f79faa196b5127af
mishevski-k <mishevski0800@gmail.com>
2023-03-18 13:15:26 +0100

    Notes backend port change gotten from env file

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -89,7 +89,7 @@ const unknownEnpoint = (request, response) => {
 
 app.use(unknownEnpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
