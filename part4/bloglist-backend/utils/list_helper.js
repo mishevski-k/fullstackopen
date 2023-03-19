cc4110f74548fa1eaa2a94afd28c3a2884d65916
mishevski-k <mishevski0800@gmail.com>
2023-03-19 20:29:00 +0100

    Part4 exercise 4.4 added test and test helpers for total likes of blogs

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1;
 };
 
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce((accumulator, blog) => {
+            return accumulator + blog.likes;
+        }, 0);
+};
+
 module.exports ={
     dummy,
+    totalLikes,
 };
