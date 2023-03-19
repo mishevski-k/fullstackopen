89f8b77c4198a820d5fb29ea9b890d257169ec26
mishevski-k <mishevski0800@gmail.com>
2023-03-19 19:12:56 +0100

    Part4 exercise 4.2 refactored bloglist backend to a more structured design

---
+++ part4/bloglist-backend/utils/middleware.js
@@ -0,0 +1,28 @@
+const logger = require('./logger');
+
+const requestLogger = (request, response, next) => {
+    logger.info(`${request.method}: ${request.path} `, request.body);
+    next();
+};
+
+const uknownEndpoint = (request, response) => {
+    response.status(404).json({error: 'uknown endpoint'}); 
+};
+
+const errorHanler = (error, request, response, next) => {
+    logger.error(error.message);
+
+    if(error.name === 'CastingError'){
+        return response.status(400).json({error: 'Malformatted id'});
+    }else if(error.name === 'ValidationError'){
+        return response.status(400).json({error: error.message});
+    }
+
+    next(error);
+};
+
+module.exports = {
+    requestLogger,
+    uknownEndpoint,
+    errorHanler
+};
